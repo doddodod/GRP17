e38a9e431ff94e203c5b862b791f77ff73524985
BoyaWang_xiaobo <44559424+doddodod@users.noreply.github.com>
2020-02-13 13:11:47 +0800

    Update ModuleFrame1_View.py
    cant push from the local

--- ModuleFrame1_View.py
+++ ModuleFrame1_View.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import QFrame, QApplication
 from resources.teacherUIPY.basicStructure_frame1 import Ui_Frame
 import sys
@@ -9,21 +10,30 @@ from ModuleFrame_Delegate import moduleFrame_Deletagte
 
 class moduleFrame1_view(QFrame, Ui_Frame):
 
+    # send signal to module page
+    enterSessionPage_SignalToPage = pyqtSignal()
+
     def __init__(self):
         # setup UI
         super(moduleFrame1_view, self).__init__()
         self.setupUi(self)
-        self.listView.clicked.connect(self.clickedList)
 
     def refresh(self):
         self.frameName_TBS.setText("Module")
         self.search_lineEdit.hide()
         self.search_toolButton.hide()
         self.addButton.hide()
+        self.listView.doubleClicked.connect(self.doubleClicked)
+        self.listView.clicked.connect(self.goSession)
 
-    def clickedList(self, qModelIndex):
+    def goSession(self, qModelIndex):
         # TODO: jump to the page
-        print("you choosed "+ str(qModelIndex.row()))
+        print("go to " + str(qModelIndex.row()))
+        self.enterSessionPage_SignalToPage.emit()
+
+
+    def doubleClicked(self, qModelIndex):
+        print("you choosed " + str(qModelIndex.row()))
 
     def print(self):
         print("print recording")
