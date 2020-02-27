9258448d0e83cd20091524442650add8aa9537fa
branch <branchqing3@gmail.com>
2020-02-27 18:49:36 +0800

    Add forgetPw Dialog, add ui files of recordedSession and stateEdit dialog, finish line view. Todo: connect two new pages to current system.

--- oneStudentFrame_View.py
+++ oneStudentFrame_View.py
@@ -1,13 +1,21 @@
 from PyQt5.QtWidgets import QFrame, QApplication, QMainWindow
+from PyQt5.QtCore import pyqtSignal
 from resources.teacherUIPY.studentInfo_frame1 import Ui_Frame
 
 
 class oneStudentFrame_view(QFrame, Ui_Frame):
 
+    # send signal to student search page
+    enterAttendancePage_SignalToPage = pyqtSignal()
+
     def __init__(self):
         super(oneStudentFrame_view, self).__init__()
         self.setupUi(self)
 
+    def goAttendance(self, qModelIndex):
+        # TODO: jump to the page
+        print("go to " + str(qModelIndex.row()))
+        self.enterSessionPage_SignalToPage.emit()
 
 # test code
 if __name__ == "__main__":
